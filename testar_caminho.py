30da4ff6abb387d16e151e8307f1c0b4ce43663e
Jaqueline Santos <jaqueline.santos@clami.com.br>
2025-12-05 15:57:49 -0300

    Mover Excel para pasta data e corrigir caminho

--- testar_caminho.py
+++
@@ -1,8 +0,0 @@
-import os
-
-caminho = r"P:\22_MOSTRUARIO DIGITAL\TABELA.xlsx"
-
-print("Existe?", os.path.exists(caminho))
-print("Diretório atual:", os.getcwd())
-print("Listando arquivos da pasta:")
-print(os.listdir(r"P:\22_MOSTRUARIO DIGITAL"))
